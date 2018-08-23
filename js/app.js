9c417325d3f8f274561c6ad42e9a07478293b8c2
Salma Mohammed <sm.71175@gmail.com>
2018-08-23 13:27:28 +0200

    App.config.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 var app = angular.module('ReaderApp', ['ngRoute']);
+
+app.config(function ($routeProvider) { 
+  $routeProvider 
+    .when('/book', { 
+      controller: 'BookshelfController', 
+      templateUrl: 'views/bookshelf.html' 
+    })
+    .otherwise({ 
+      redirectTo: '/books' 
+    }); 
+});
